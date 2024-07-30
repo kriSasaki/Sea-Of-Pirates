9ceb2dbe51f1ac55dfd635c4c5f6d3148c194688
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2024-07-31 02:49:39 +0300

    Add PlayerHold

---
+++ Assets/_Project/Scripts/Players/ResourcesHold/PlayerHold.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Project.Configs.GameResources;
+using UnityEngine;
+
+namespace Project.Players.ResourcesHold
+{
+    public class PlayerHold : MonoBehaviour
+    {
+        private int _cargoSize;
+        private List<GameResource> _cargo;
+        
+    }
+}
