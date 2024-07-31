3375a4173dfc6500b349cf3a76ca818c34aed38c
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2024-07-31 04:57:58 +0300

    Update PlayerHold

--- Assets/_Project/Scripts/Players/ResourcesHold/PlayerHold.cs
+++ Assets/_Project/Scripts/Players/ResourcesHold/PlayerHold.cs
@@ -1,14 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
-using Project.Configs.GameResources;
-using UnityEngine;
+using Project.Systems.Stats;
 
 namespace Project.Players.ResourcesHold
 {
-    public class PlayerHold : MonoBehaviour
+    public class PlayerHold
     {
         private int _cargoSize;
-        private List<GameResource> _cargo;
-        
+        private List<GameResourceAmount> _cargo;
+
+        public void AddResource(GameResourceAmount gameResourceAmount)
+        {
+            if (_cargo.Count < _cargoSize)
+            {
+                _cargo.Add(gameResourceAmount);
+            }
+        }
+
+        public void RemoveResources()
+        {
+            _cargo.Clear();
+        }
+
+        public void UpgradeHold()
+        {
+            _cargoSize++;
+        }
+
+        public int GetResourcesAmount()
+        {
+            int resourcesAmount = 0;
+            
+            for (int i = 0; i < _cargo.Count; i++)
+            {
+                resourcesAmount += _cargo[i].Amount;
+            }
+
+            return resourcesAmount;
+        }
     }
 }
