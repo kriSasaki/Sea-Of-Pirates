24288174a023f64fb4c34b595c2a8746a84ca671
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2024-08-22 18:56:04 +0300

    Add AOE Plugin

--- Assets/_Project/Scripts/Players/PlayerLogic/PlayerAttack.cs
+++ Assets/_Project/Scripts/Players/PlayerLogic/PlayerAttack.cs
@@ -5,29 +5,69 @@ using Project.Enemies;
 using Project.Players.Inputs;
 using Zenject;
 using Project.Interfaces.Stats;
+using System;
+using System.Linq;
 
 namespace Project.Players.PlayerLogic
 {
     public class PlayerAttack : MonoBehaviour
     {
         [SerializeField] private SphereCollider _attackZone;
+        [SerializeField] private float _attackCooldown = 2f;
 
         private IPlayerStats _playerStats;
+
+
+        private List<Enemy> _enemiesInZone = new();
+        private Coroutine _battleCoroutine;
+        private WaitForSeconds _attackDelay;
 
         public int Damage => _playerStats.Damage;
         public int AttackRange => _playerStats.AttackRange;
         public int CannonsAmount => _playerStats.CannonsAmount;
 
+        private bool IsEnemiesInZone => _enemiesInZone.Count > 0;
+        private bool IsInBattle => _battleCoroutine != null;
+
         private void Start()
         {
-
-
             SetAttackZone();
+        }
+
+        private void OnDestroy()
+        {
+            _playerStats.StatsUpdated -= OnStatsUpdated;
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            
+            if (other.TryGetComponent<Enemy>(out Enemy enemy))
+            {
+                _enemiesInZone.Add(enemy);
+
+                if (IsInBattle == false)
+                    EnterBattle();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent<Enemy>(out Enemy enemy))
+            {
+                _enemiesInZone.Remove(enemy);
+
+                if (IsEnemiesInZone == false)
+                    ExitBattle();
+            }
+        }
+
+        [Inject]
+        private void Construct(IPlayerStats playerStats)
+        {
+            _playerStats = playerStats;
+            _playerStats.StatsUpdated += OnStatsUpdated;
+
+            _attackDelay = new WaitForSeconds(_attackCooldown);
         }
 
         private void SetAttackZone()
@@ -35,12 +75,61 @@ namespace Project.Players.PlayerLogic
             _attackZone.radius = AttackRange;
         }
 
-        [Inject]
-        public void Construct(IPlayerStats playerStats)
+        private void EnterBattle()
         {
-            _playerStats = playerStats;
+            _battleCoroutine = StartCoroutine(Battle());
         }
 
+        private void ExitBattle()
+        {
+            ClearBattleCoroutine();
+        }
 
+        private IEnumerator Battle()
+        {
+            while (IsEnemiesInZone)
+            {
+                yield return _attackDelay;
+                
+                foreach (Enemy enemy in GetTargetEnemies())
+                {
+                    enemy.TakeDamage(Damage);
+                }
+
+                yield return null;
+                CheckEnemies();
+            }
+
+            ExitBattle();
+        }
+
+        private IEnumerable<Enemy> GetTargetEnemies()
+        {
+            return _enemiesInZone
+                .OrderBy(e => Vector3.SqrMagnitude(e.transform.position - transform.position))
+                .Take(Mathf.Min(_enemiesInZone.Count, CannonsAmount));
+        }
+
+        private void CheckEnemies()
+        {
+            for (int i = _enemiesInZone.Count - 1; i >= 0; i--)
+            {
+                if (_enemiesInZone[i] == null)
+                    _enemiesInZone.RemoveAt(i);
+            }
+        }
+
+        private void ClearBattleCoroutine()
+        {
+            if (_battleCoroutine != null)
+                StopCoroutine(_battleCoroutine);
+
+            _battleCoroutine = null;
+        }
+
+        private void OnStatsUpdated()
+        {
+            SetAttackZone();
+        }
     }
 }
