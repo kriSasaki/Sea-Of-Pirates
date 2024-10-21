76ba43f9995ef006857e2d13edfc0d5f7b490211
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2024-10-21 09:27:59 +0300

    Update GameData.cs

--- Assets/_Project/Scripts/Systems/Data/GameData.cs
+++ Assets/_Project/Scripts/Systems/Data/GameData.cs
@@ -10,10 +10,10 @@ namespace Scripts.Systems.Data
     {
         private const int InitialStatLevel = 1;
 
-        [SerializeField] private List<GameResourceData> StorageData = new List<GameResourceData>();
-        [SerializeField] private List<PlayerStatData> PlayerStatsLevels = new List<PlayerStatData>();
-        [SerializeField] private List<QuestData> Quests = new List<QuestData>();
-        [SerializeField] private List<LevelData> Levels = new List<LevelData>();
+        [SerializeField] private List<GameResourceData> _storageData = new List<GameResourceData>();
+        [SerializeField] private List<PlayerStatData> _playerStatsLevels = new List<PlayerStatData>();
+        [SerializeField] private List<QuestData> _quests = new List<QuestData>();
+        [SerializeField] private List<LevelData> _levels = new List<LevelData>();
         [SerializeField] private string _currentScene;
         [SerializeField] private bool _isAddHided = false;
         [SerializeField] private int _score = 0;
@@ -28,12 +28,12 @@ namespace Scripts.Systems.Data
 
         public GameResourceData GetResourceData(string id)
         {
-           return StorageData.FirstOrDefault(r => r.ID == id);
+           return _storageData.FirstOrDefault(r => r.ID == id);
         }
 
         public void UpdateResourceData(string id, int value)
         {
-            GameResourceData data = StorageData.FirstOrDefault(r => r.ID == id);
+            GameResourceData data = _storageData.FirstOrDefault(r => r.ID == id);
 
             if (data != null)
             {
@@ -41,18 +41,18 @@ namespace Scripts.Systems.Data
             }
             else
             {
-                StorageData.Add(new GameResourceData(id, value));
+                _storageData.Add(new GameResourceData(id, value));
             }
         }
 
         public PlayerStatData GetPlayerStatData(StatType statType)
         {
-            PlayerStatData playerStatData = PlayerStatsLevels.FirstOrDefault(s => s.StatType == statType);
+            PlayerStatData playerStatData = _playerStatsLevels.FirstOrDefault(s => s.StatType == statType);
 
             if (playerStatData == null)
             {
                 playerStatData = new PlayerStatData(statType, InitialStatLevel);
-                PlayerStatsLevels.Add(playerStatData);
+                _playerStatsLevels.Add(playerStatData);
             }
 
             return playerStatData;
@@ -60,7 +60,7 @@ namespace Scripts.Systems.Data
 
         public void UpdateStatData(StatType type, int level)
         {
-            PlayerStatData data = PlayerStatsLevels.FirstOrDefault(s => s.StatType == type);
+            PlayerStatData data = _playerStatsLevels.FirstOrDefault(s => s.StatType == type);
 
             if (data != null)
             {
@@ -69,7 +69,7 @@ namespace Scripts.Systems.Data
             else
             {
                 var statData = new PlayerStatData(type, level);
-                PlayerStatsLevels.Add(statData);
+                _playerStatsLevels.Add(statData);
             }
         }
 
@@ -77,7 +77,7 @@ namespace Scripts.Systems.Data
         {
             var quests = new Dictionary<string, QuestStatus>();
 
-            foreach (QuestData questData in Quests)
+            foreach (QuestData questData in _quests)
             {
                 quests.Add(questData.ID, new QuestStatus(questData.State, questData.Progress));
             }
@@ -87,24 +87,24 @@ namespace Scripts.Systems.Data
 
         public void UpdateQuestData(string id, QuestStatus status)
         {
-            if (Quests.Any(q => q.ID == id) == false)
+            if (_quests.Any(q => q.ID == id) == false)
             {
-                Quests.Add(new QuestData(id, status));
+                _quests.Add(new QuestData(id, status));
             }
 
-            QuestData data = Quests.Find(q => q.ID == id);
+            QuestData data = _quests.Find(q => q.ID == id);
             data.Update(status);
         }
 
         public LevelData GetLevelData(string levelName)
         {
-            LevelData leveldata = Levels.FirstOrDefault(l => l.LevelName == levelName);
+            LevelData leveldata = _levels.FirstOrDefault(l => l.LevelName == levelName);
 
             if (leveldata == null)
             {
                 leveldata = new LevelData(levelName);
 
-               Levels.Add(leveldata);
+               _levels.Add(leveldata);
             }
 
             return leveldata;
